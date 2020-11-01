16170affb7c9f6053539c4531108762248b983a6
AndreCosta101 <andre.costa101@gmail.com>
2020-10-31 22:04:04 -0300

    Páginas estáticas dinâmicas

--- src/pages/top10.tsx
+++ src/pages/top10.tsx
@@ -28,7 +28,7 @@ export default function Top10({ products }: ITop10Props) {
 }
 
 
-//server side rendering
+//Static Site Generation
 export const getStaticProps: GetStaticProps<ITop10Props> = async () => {
   const response = await fetch('http://localhost:3333/products')
   const products = await response.json();
